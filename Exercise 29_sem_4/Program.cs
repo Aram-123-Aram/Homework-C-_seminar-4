bb4f9af5fbc4b8d4596755ced41074737acb4691
Aram <armsir-8182@mail.ru>
2022-10-30 12:36:45 +0300

    Filling and showing the massiv with 8 numbers from 0 to 100

--- Exercise 29_sem_4/Program.cs
+++ Exercise 29_sem_4/Program.cs
@@ -1,4 +1,49 @@
 ﻿/* Задача 29: 
 Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
-6, 1, 33 -> [6, 1, 33]
+6, 1, 33 -> [6, 1, 33]*/
+
+// 1. Variant:
+int[] array = new int[8];
+
+int[] FullArray(int[] mas1)
+{
+    for (int i = 0; i < mas1.Length; i++) {mas1[i] = new Random().Next(0,100); }
+    return mas1;
+}
+
+FullArray(array);
+foreach(var item in array) {Console.Write($" {item},  "); }
+
+/* 2. Variant-vozvrashaemaya fynktia
+int[] array = new int[8];
+
+int[] FullArray(int[] mas1)
+{
+    for (int i = 0; i < mas1.Length; i++) {mas1[i] = new Random().Next(0,100); }
+    return mas1;
+}
+int[] ShowArray(int[] mas2)
+{
+    for(int i = 0; i < mas2.Length; i++) {Console.WriteLine($"{mas2[i]} "); }
+    return mas2;
+}
+FullArray(array);
+ShowArray(array);*/
+
+/*3. Variant - nevozvrashaemaya funktia
+
+int[] array = new int[8];
+
+void FullArray(int[] mas1)
+{
+    for (int i = 0; i < mas1.Length; i++) {mas1[i] = new Random().Next(0,100); }
+}
+void ShowArray(int[] mas2)
+{
+    for(int i = 0; i < mas2.Length; i++) {Console.WriteLine($"{mas2[i]} "); }
+}
+FullArray(array);
+ShowArray(array);*/
+
+
