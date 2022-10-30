3396379976132b03c20aa8418dc236817edad4cb
Aram <armsir-8182@mail.ru>
2022-10-30 12:11:19 +0300

    Solving the Exercise 27 - to find the summa of numbers including in number n

---
+++ Exercise 27_sem_4/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 27: 
+Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.  452 -> 11
+                                                                                  82 -> 10
+                                                                                  9012 -> 12 */
+Console.Write("Enter any number: n= ");
+int.TryParse(Console.ReadLine()!, out int n);
+
+Console.Write($"The summa of numbers n equals: -----{n} -> {Funct_sum(n)}");
+
+int Funct_sum(int n)
+{
+    int temp = n;
+    int Sum_numbers = 0;
+    while(temp != 0)
+    {Sum_numbers =Sum_numbers + temp%10; temp = temp/10;}
+    return Sum_numbers;
+
+}
+Funct_sum(n);
